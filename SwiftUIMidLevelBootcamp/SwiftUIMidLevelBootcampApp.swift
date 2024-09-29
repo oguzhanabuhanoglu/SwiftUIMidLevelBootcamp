c8f2f8fe7f6dc23fff5267c6e82715597fcaa6e4
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-09-29 15:35:06 +0300

    First commit

---
+++ SwiftUIMidLevelBootcamp/SwiftUIMidLevelBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMidLevelBootcampApp.swift
+//  SwiftUIMidLevelBootcamp
+//
+//  Created by Oğuzhan Abuhanoğlu on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMidLevelBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
