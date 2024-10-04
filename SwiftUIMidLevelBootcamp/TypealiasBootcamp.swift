2b8aa8178a7da4ff4e44093dc56e5a4a2e2c291c
oguzhanabuhanoglu <oguzhanabuhanoglu@gmail.com>
2024-10-04 11:53:35 +0300

    TypaliasBootcamp

---
+++ SwiftUIMidLevelBootcamp/TypealiasBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TypealiasBootcamp.swift
+//  SwiftUIMidLevelBootcamp
+//
+//  Created by Oğuzhan Abuhanoğlu on 4.10.2024.
+//
+
+import SwiftUI
+
+struct TypealiasBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypealiasBootcamp()
+}
